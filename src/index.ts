2c0d45f106d23d0dd011ee0633e567ef5e50f26e
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-09-11 21:31:44 +0200

    add azure iot hub

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("hello world")
+import { Client, Message, DeviceMethodRequest, DeviceMethodResponse } from "azure-iot-device"
+import { Mqtt } from "azure-iot-device-mqtt"
+import { exec } from "child_process"
+
+let deviceClient: Client = Client.fromConnectionString(process.env['ConnectionString']!, Mqtt);
+
+function showError(err?: Error | undefined) {
+    if (err) {
+        console.error(`Error sending response: ${err}`)
+    }
+}
+
+function execAndResponse(argument: string, description: string, response: DeviceMethodResponse) {
+    console.log(description);
+    exec(`steuerung ${argument}`, (err) => {
+        if (err) {
+            let msg = `faild to send ${description}. Error: "${err}"`
+            console.error(msg)
+            response.send(500, msg, showError);
+        }
+        else
+        {
+            response.send(200, `send ${description} to the plug socket`, showError);
+        }
+    })
+
+}
+
+deviceClient.onDeviceMethod('onSwitchOn', (request, response) => execAndResponse('1', 'power on', response))
+deviceClient.onDeviceMethod('onSwitchOff', (request, response) => execAndResponse('0', 'power off', response))
+
+console.log('Device connect to iot hub')

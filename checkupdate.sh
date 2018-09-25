b5d192b2ee0da30b201f5d99b5c226ac974484fb
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-09-25 20:45:00 +0200

    add check script for cron

---
+++ checkupdate.sh
@@ -0,0 +1,10 @@
+cd $(dirname "$0")
+/usr/local/bin/docker-compose pull > /dev/null 2>&1
+config=$(/usr/local/bin/docker-compose config)
+container=$(echo -e "$config" | sed -n "s/    container_name: \(.*\)/\1/p")
+image=$(echo -e "$config" | sed -n "s/    image: \(.*\)/\1/p")
+running=$(docker inspect  --format "{{.Image}}" $container)
+latest=$(docker inspect --format "{{.Id}}" $image)
+if [ "$running" != "$latest" ]; then
+  echo "espresso needs update"
+fi

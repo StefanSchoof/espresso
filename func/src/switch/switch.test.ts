302366155ac593349cbf80bf8ac4951d11116353
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-10-04 21:42:23 +0200

    create basic azure function

---
+++ func/src/switch/switch.test.ts
@@ -0,0 +1,10 @@
+import { run } from './switch';
+import { Context, HttpMethod } from 'azure-functions-ts-essentials';
+
+test('returns Hello World', async () => {
+    const context = {res: {status: 0, body: ''}} as Context;
+
+    await run(context, {method: HttpMethod.Get});
+
+    expect(context.res!.body).toBe('Hello World');
+  });
---
+++ func/src/switch/switch.ts
@@ -0,0 +1,8 @@
+import { Context, HttpRequest } from 'azure-functions-ts-essentials';
+
+export async function run(context: Context, req: HttpRequest): Promise<void> {
+    context.res = {
+        status: 200,
+        body: "Hello World"
+    }
+  }

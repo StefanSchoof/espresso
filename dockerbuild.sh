8866e5a520eb295559661ff672953800c5e8d605
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-10-07 17:58:32 +0200

    tag branches and cache build

---
+++ dockerbuild.sh
@@ -0,0 +1,26 @@
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo "##vso[task.setvariable variable=branch]$branch"
+targets=( node cppbuilder builder )
+image="stefanschoof/espresso"
+if [[ ! $(uname -m) == arm* ]]
+then
+  docker run --rm --privileged multiarch/qemu-user-static:register --reset
+fi
+for target in "${targets[@]}"
+do
+  tag=$image:build-cache-$target
+  docker pull $tag
+  cachefrom+="--cache-from $tag "
+  docker build --target $target \
+    $cachefrom\
+    -t $tag .
+done
+if [[ ! -z "$BUILD_BUILDID" ]]
+then
+  buildtag="-t $image:build$BUILD_BUILDID "
+fi
+
+docker build $cachefrom \
+  --cache-from $image:$branch \
+  $buildtag \
+  -t $image:$branch .

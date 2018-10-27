0191b672a2e8f103cd43c244389d5734ed606c58
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-10-27 17:26:33 +0000

    use azure iot sdk 1.8 with promise support

--- node/src/index.test.ts
+++ node/src/index.test.ts
@@ -35,7 +35,7 @@ describe('index', () => {
 
   test('on switch on the on cmd is called', async () => {
     const response = {
-      send: jest.fn((code, payload, done) => done())
+      send: jest.fn((code, payload) => Promise.resolve())
     };
 
     await mockDeviceMethods.onSwitchOff({}, response);
@@ -43,12 +43,12 @@ describe('index', () => {
     expect(exec)
       .toHaveBeenCalledWith('steuerung 0', expect.any(Function));
     expect(response.send)
-      .toHaveBeenLastCalledWith(200, expect.anything(), expect.anything());
+      .toHaveBeenLastCalledWith(200, expect.anything());
   });
 
   test('on switch off the on cmd is called', async () => {
     const response = {
-      send: jest.fn((code, payload, done) => done())
+      send: jest.fn((code, payload) => Promise.resolve())
     };
 
     await mockDeviceMethods.onSwitchOn({}, response);
@@ -56,12 +56,12 @@ describe('index', () => {
     expect(exec)
       .toHaveBeenCalledWith('steuerung 1', expect.any(Function));
     expect(response.send)
-      .toHaveBeenLastCalledWith(200, expect.anything(), expect.anything());
+      .toHaveBeenLastCalledWith(200, expect.anything());
   });
 
   test('return an error if exec fails', async () => {
     const response = {
-      send: jest.fn((code, payload, done) => done())
+      send: jest.fn((code, payload) => Promise.resolve())
     };
     (exec as any as jest.Mock<any>)
       .mockImplementationOnce((cmd, cb) => cb(new Error('Command failed')));
@@ -69,7 +69,7 @@ describe('index', () => {
     await mockDeviceMethods.onSwitchOff({}, response);
 
     expect(response.send)
-      .toHaveBeenLastCalledWith(500, expect.anything(), expect.anything());
+      .toHaveBeenLastCalledWith(500, expect.anything());
     expect(appInsights.defaultClient.trackException)
       .toHaveBeenLastCalledWith({exception: new Error('Command failed')});
   });
@@ -77,7 +77,7 @@ describe('index', () => {
   test('log an error if response fails', async () => {
     const e = new Error('Send response failed');
     const response = {
-      send: jest.fn((code, payload, done) => done(e))
+      send: jest.fn((code, payload) => Promise.reject(e))
     };
 
     await mockDeviceMethods.onSwitchOff({}, response);
@@ -86,25 +86,10 @@ describe('index', () => {
       .toHaveBeenLastCalledWith({exception: new Error('Send response failed')});
   });
 
-  test('does not destroy context in response send', async () => {
-    const response = {
-      isResponseComplete: false,
-      send: jest.fn(function(this: any, code: any, payload: any, done: any): void {
-        if (!this.isResponseComplete) {
-          done();
-        }
-      })
-    };
-
-    await expect(mockDeviceMethods.onSwitchOff({}, response))
-      .resolves
-      .toBe(undefined);
-  });
-
   test('track request on respose error', async () => {
     const e = new Error('Send response failed');
     const response = {
-      send: jest.fn((code, payload, done) => done(e))
+      send: jest.fn((code, payload) => Promise.reject(e))
     };
 
     await mockDeviceMethods.onSwitchOff({}, response);

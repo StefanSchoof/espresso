dbd98c4565dffbbe95722923d4b7b43524321b89
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-10-28 17:17:58 +0100

    Deploy to pi (#19)

---
+++ node/pideploy.sh
@@ -0,0 +1,8 @@
+cd espresso
+# put secure varibales into env file
+cat > .env << EOF
+CONNECTION_STRING=$1
+APPINSIGHTS_INSTRUMENTATIONKEY=$2
+EOF
+# docker compose use stderr for normal messages and devops shows them as errors
+TAG=build$3 docker-compose --no-ansi up -d 2>&1

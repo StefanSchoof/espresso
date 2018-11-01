c3b7dcd19f780c8aa320c6b0730e53d949fad016
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-11-01 20:53:37 +0000

    add keyvault secret

--- terraform/create.sh
+++ terraform/create.sh
@@ -2,19 +2,27 @@
 terraform apply
 resource_group=$(terraform output resource_group)
 storage_account=$(terraform output storage_account)
+
+# currently not supported in terraform, see https://github.com/terraform-providers/terraform-provider-azurerm/issues/1903
 az extension add --name storage-preview
 if [ "$(az storage blob service-properties show --account-name $storage_account --query 'staticWebsite.enabled')" = "false" ];
 then
-    az storage blob service-properties update --account-name $storage_account --static-website --index-document index.html
+    echo "activate static web"
+    az storage blob service-properties update --account-name $storage_account --static-website --index-document index.html > /dev/null
 fi
 websiteUrl=$(az storage account show -n $storage_account -g $resource_group --query "primaryEndpoints.web" --output tsv)
+echo "websiteUrl: $websiteUrl"
 
-azurerm_iothub=$(terraform output azurerm_iothub)
+# currently not supported in terraform, see https://github.com/terraform-providers/terraform-provider-azurerm/issues/1712
+iothub=$(terraform output iothub)
 deviceId="espressoPi"
 az extension add --name azure-cli-iot-ext
-if ! az iot hub device-identity show --hub-name $azurerm_iothub --device-id $deviceId
+if ! az iot hub device-identity show --hub-name $iothub --device-id $deviceId > /dev/null
 then
-    az iot hub device-identity create --hub-name $azurerm_iothub --device-id $deviceId
+    echo "Add $deviceId to iot hub"
+    az iot hub device-identity create --hub-name $iothub --device-id $deviceId
 fi
-deviceConnectionString=$(az iot hub device-identity show-connection-string --hub-name $azurerm_iothub --device-id $deviceId --output tsv)
-serviceConnectionString=$(az iot hub show-connection-string --hub-name $azurerm_iothub --output tsv)
+deviceConnectionString=$(az iot hub device-identity show-connection-string --hub-name $iothub --device-id $deviceId --output tsv)
+
+function_app=$(terraform output function_app)
+echo "../depoly.sh -s $storage_account -f $function_app"
--- terraform/espresso.tf
+++ terraform/espresso.tf
@@ -12,8 +12,8 @@ locals {
   stage = "${var.env == "production" ? "" : "${var.env}" }"
 }
 
+data "azurerm_client_config" "current" {}
 
-# Create a resource group
 resource "azurerm_resource_group" "espresso" {
   name     = "espresso${local.stage}"
   location = "West Europe"
@@ -57,20 +57,69 @@ resource "azurerm_function_app" "espressoPi" {
   resource_group_name       = "${azurerm_resource_group.espresso.name}"
   app_service_plan_id       = "${azurerm_app_service_plan.WestEuropePlan.id}"
   storage_connection_string = "${azurerm_storage_account.espressopi.primary_connection_string}"
+  identity {
+    type = "SystemAssigned"
+  }
+
   app_settings {
     WEBSITE_RUN_FROM_PACKAGE = "1"
+    WEBSITE_NODE_DEFAULT_VERSION = "8.11.1"
+    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.function.instrumentation_key}"
   }
+  lifecycle {
+    ignore_changes = [
+      "app_settings.%",
+      "app_settings.WEBSITE_RUN_FROM_ZIP", # Done by the function depolyment
+    ]
+  }
+  version = "~2"
 }
 
 resource "azurerm_key_vault" "keyvault" {
   name                        = "espresso${local.stage}Vault"
   location                    = "${azurerm_resource_group.espresso.location}"
   resource_group_name         = "${azurerm_resource_group.espresso.name}"
-  tenant_id                   = "${var.aad_tenant_id}"
+  tenant_id                   = "${data.azurerm_client_config.current.tenant_id}"
+
+   access_policy {
+    tenant_id = "${azurerm_function_app.espressoPi.identity.0.tenant_id}"
+    object_id = "${azurerm_function_app.espressoPi.identity.0.principal_id}"
+
+    key_permissions = [
+    ]
+
+    secret_permissions = [
+      "get",
+    ]
+  }
+
+  access_policy {
+    tenant_id = "${data.azurerm_client_config.current.tenant_id}"
+    object_id = "${var.aad_user_id}"
+
+    key_permissions = [
+    ]
+
+    secret_permissions = [
+      "get",
+      "list",
+      "set",
+      "delete",
+      "recover",
+      "backup",
+      "restore",
+    ]
+  }
 
   sku {
     name = "standard"
   }
+}
+
+resource "azurerm_key_vault_secret" "iotHubConnectionString" {
+  name      = "iotHubConnectionString"
+  value     = "HostName=${azurerm_iothub.espresso.hostname};SharedAccessKeyName=${azurerm_iothub.espresso.shared_access_policy.0.key_name};SharedAccessKey=${azurerm_iothub.espresso.shared_access_policy.0.primary_key}"
+  vault_uri = "${azurerm_key_vault.keyvault.vault_uri}"
 }
 
 resource "azurerm_application_insights" "node" {
@@ -102,6 +151,10 @@ output "storage_account" {
   value = "${azurerm_storage_account.espressopi.name}"
 }
 
-output "azurerm_iothub" {
+output "iothub" {
   value = "${azurerm_iothub.espresso.name}"
+}
+
+output "function_app" {
+  value = "${azurerm_function_app.espressoPi.name}"
 }

66f74d976c3e505ed0fca25462cc67bb0477f87f
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-11-04 22:04:39 +0000

    add terraform build script

--- terraform/terraformfunctions.sh
+++ terraform/terraformfunctions.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -e
 function terraform {
-  docker run --env-file azurerm.env -e TF_IN_AUTOMATION=true -v $(pwd):/app/ -w /app/ hashicorp/terraform $@
+  docker run \
+    --env-file azurerm.env \
+    -e TF_IN_AUTOMATION=true \
+    -e TF_WORKSPACE \
+    -v $(pwd):/app/ \
+    -w /app/ \
+    hashicorp/terraform $@
 }

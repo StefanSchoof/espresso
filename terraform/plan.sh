99299b004ecd4c466ccf6cf31b7d6d44293e35c5
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-11-05 16:52:57 +0000

    rename and no color

--- terraform/plan.sh
+++ terraform/plan.sh
@@ -6,6 +6,6 @@ terraform --version
 for workspace in $(terraform workspace list | sed 's/. //' | grep -v -e "default" -e "^$")
 do
   echo "plan $workspace"
-  TF_WORKSPACE=$workspace terraform plan -input=false
+  TF_WORKSPACE=$workspace terraform plan -input=false -no-color
 done
 

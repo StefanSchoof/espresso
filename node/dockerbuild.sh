ba8581f059e5566ac8777d4a12e5386a9bd09fe1
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-08 10:53:04 +0000

    go back to build without manifest (will use qemu for testing)

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -4,59 +4,30 @@ branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})
 echo "##vso[task.setvariable variable=branchname]$branch"
 targets=( node cppbuilder builder )
 image="stefanschoof/espresso"
-if [[ ! -z "$BUILD_BUILDID" ]]
-then
-  buildtagarm="-t $image:build${BUILD_BUILDID}_arm "
-  buildtagx86="-t $image:build${BUILD_BUILDID}_amd64 "
-fi
-
+# enable cross compile with on not arm devices
 if [[ ! $(uname -m) == arm* ]]
 then
-  docker build -f Dockerfile_amd64 -t $image:${branch}_amd64 $buildtagx86 .
-  # enable cross compile with on not arm devices
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
 fi
 
-if [[ ! -z "$USECACHEFROM" ]]
+# cache-from does not work with multistage, see https://github.com/moby/moby/issues/34715
+for target in "${targets[@]}"
+do
+  tag=$image:build-cache-$target
+  docker pull $tag
+  cachefrom+="--cache-from $tag "
+  docker build --target $target \
+    $cachefrom\
+    -t $tag .
+done
+
+docker pull $image:$branch
+
+if [[ ! -z "$BUILD_BUILDID" ]]
 then
-  cachefrom="--cache-from $image:$branch "
-  # cache-from does not work with multistage, see https://github.com/moby/moby/issues/34715
-  for target in "${targets[@]}"
-  do
-    tag=$image:build-cache-$target
-    docker pull $tag
-    cachefrom+="--cache-from $tag "
-    docker build --target $target \
-      $cachefrom\
-      -t $tag .
-    if [[ ! -t "$PUSH" ]]
-    then
-      echo docker push $tag
-    fi
-  done
-
-  docker pull $image:$branch
+  buildtag="-t $image:build$BUILD_BUILDID "
 fi
-
-docker build \
+docker build --cache-from $image:$branch \
   $cachefrom \
-  $buildtagarm \
-  -t $image:${branch}_arm .
-
-if [[ ! -z "$PUSH" ]]
-then
-  docker push $image:${branch}_arm
-  docker push $image:${branch}_amd64
-  docker push $image:build${BUILD_BUILDID}_arm
-  docker push $image:build${BUILD_BUILDID}_amd64
-  export DOCKER_CLI_EXPERIMENTAL=enabled
-  docker manifest create --amend $image:${branch} $image:${branch}_arm $image:${branch}_amd64
-  docker manifest annotate $image:${branch} $image:${branch}_amd64 --os linux --arch amd64
-  docker manifest annotate $image:${branch} $image:${branch}_arm --os linux --arch arm
-  docker manifest push $image:${branch}
-#  docker pull $image:${branch}
-  docker manifest create --amend $image:build${BUILD_BUILDID} $image:build${BUILD_BUILDID}_arm $image:build${BUILD_BUILDID}_amd64
-  docker manifest annotate $image:build${BUILD_BUILDID} $image:build${BUILD_BUILDID}_amd64 --os linux --arch amd64
-  docker manifest annotate $image:build${BUILD_BUILDID}  $image:build${BUILD_BUILDID}_arm --os linux --arch arm
-  docker manifest push $image:build${BUILD_BUILDID}
-fi
+  $buildtag \
+  -t $image:$branch .

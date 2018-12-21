c62254cce546ee3cf104cdd7686ff2c645857ff3
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-21 05:07:14 +0000

    export workspace var

--- deploy/destroy.sh
+++ deploy/destroy.sh
@@ -3,7 +3,7 @@ set -e
 . bashfunctions.sh
 
 # destroy costly resouces in test
-TF_WORKSPACE="test"
+export TF_WORKSPACE="test"
 terraform init -backend-config=/temp/backend.conf -input=false
 terraform destroy -target azurerm_virtual_machine.dockerhost -target azurerm_iothub.iothub -auto-approve
 

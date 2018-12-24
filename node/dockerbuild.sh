407675bc99167477f5d4bedeee0b649bd74cd7b0
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-24 15:16:54 +0100

    Use Alpine images (#43)

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 set -e
+function pullarmimage {
+  # since the platform arg needs experimental daemon and there is no way do activate experimental daemon on azure pipeline => a hack...
+  imagename=${1%:*}
+  digest=$(DOCKER_CLI_EXPERIMENTAL="enabled" docker manifest inspect $1 | jq --raw-output '.manifests[] | select(.platform.architecture == "arm").digest')
+  docker pull $imagename@$digest
+  docker tag $imagename@$digest $1
+}
+
 # in azure pipeline git is in detached head so git does not know it brach and we take the env var. But these are in the format /ref/head/master, so we take the basename
 branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})
 echo "##vso[task.setvariable variable=branchname]$branch"
-targets=( node cppbuilder builder )
+targets=( cppbuilder builder )
 image="stefanschoof/espresso"
 # enable cross compile with on not arm devices
 if [[ ! $(uname -m) == arm* ]]
 then
   docker run --rm --privileged multiarch/qemu-user-static:register --reset
+  for baseimage in $(sed -n 's/^FROM \([^ ]*\) .*/\1/p' Dockerfile | sort --uniq)
+  do
+    pullarmimage $baseimage
+  done
+  targets=( qemu ${targets[@]} )
+  dockerfilearg="-f Dockerfile_x86"
+  sed 's/#x86only //' Dockerfile > Dockerfile_x86
 fi
 
 # cache-from does not work with multistage, see https://github.com/moby/moby/issues/34715
@@ -18,10 +33,11 @@ do
   if [[ ! -z "$USECACHEFROM" ]]
   then
     cachefrom+="--cache-from $tag "
-    docker pull $tag
+    docker pull $tag || true
   fi
   docker build --target $target \
-    $cachefrom\
+    $cachefrom \
+    $dockerfilearg \
     -t $tag .
   if [[ ! -z "$PUSH" ]]
   then
@@ -47,6 +63,7 @@ fi
 
 docker build \
   $cachefrom \
+  $dockerfilearg \
   $buildtag \
   -t $image:$branch .
 

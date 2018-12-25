b3fd8040c3a7790c84339f48d561affd026e6a22
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-25 21:57:32 +0000

    reduce code for terser

--- func/src/switch/switch.ts
+++ func/src/switch/switch.ts
@@ -6,7 +6,7 @@ import { promisify } from 'util';
 
 const deviceId = 'espressoPi';
 
-async function getConnectionString(): Promise<string> {
+export async function getConnectionString(): Promise<string> {
     const cred = process.env.APPSETTING_WEBSITE_SITE_NAME ?
         // cast currently needed, remove after fix for https://github.com/Azure/azure-sdk-for-node/issues/3778 is released
         msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'} as msRestAzure.MSIAppServiceOptions) :
@@ -19,40 +19,3 @@ async function getConnectionString(): Promise<string> {
         return secret.value;
     }
 }
-
-export async function run(context: HttpContext, req: IFunctionRequest): Promise<void> {
-    if (req.query.on === undefined && req.query.off === undefined) {
-        context.res = {
-            status: 404,
-            body: 'missing on or off query string'
-        };
-
-        return;
-    }
-    const connectionString = await getConnectionString();
-    const client = Client.fromConnectionString(connectionString);
-
-    // remove promisify, after https://github.com/Azure/azure-sdk-for-node/issues/3369
-    // and https://github.com/Azure/azure-iot-sdk-node/issues/362 are solved
-    // (planed by 22102018 (https://github.com/Azure/azure-sdk-for-node/milestone/35)
-    const invokeDeviceMethod = promisify<string, DeviceMethodParams, any>((d, p, cb: any) =>
-        client.invokeDeviceMethod(d, p, cb));
-
-    const methodParams = {
-        methodName: req.query.off !== undefined ? 'onSwitchOff' : 'onSwitchOn'
-    };
-    try {
-        const result = await invokeDeviceMethod(deviceId, methodParams);
-
-        context.res = {
-            status: result.status,
-            body: result.payload
-        };
-    } catch (err) {
-        context.log.error(`Failed to invoke method "${methodParams.methodName}" with error: "${err.message}"`, err);
-        context.res = {
-            status: 500,
-            body: 'Failed to invoke method'
-        };
-    }
-  }

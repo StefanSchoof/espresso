f5241cd8aee53f2fcfbecd5aca7c2a6d5a7c3dc5
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-26 07:31:02 +0000

    remove ts error

--- func/src/switch/switch.ts
+++ func/src/switch/switch.ts
@@ -6,5 +6,5 @@ export async function getConnectionString(): Promise<string> {
     const cred = msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'} as msRestAzure.MSIAppServiceOptions);
     const client = new KeyVaultClient(await cred);
     const secret = await client.getSecret(process.env.KEYVAULT_URI!, 'iotHubConnectionString', '');
-    return secret.value;
+    return secret.value!;
 }

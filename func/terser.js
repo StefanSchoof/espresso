9a90a9e0d551bf63ebdf299c42d4a87fc7f89ab2
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2018-12-27 20:14:44 +0000

    remove options from terserjs (they are not needed for repro)

--- func/terser.js
+++ func/terser.js
@@ -1,23 +1,5 @@
-const Terser = require('terser');
+const Terser = require('terser')
 const fs = require('fs')
-const options = { ecma: undefined,
-  warnings: undefined,
-  parse: {},
-  compress: {},
-  mangle: true,
-  output:
-   { shebang: true,
-     comments: /^\**!|@preserve|@license|@cc_on/i,
-     beautify: false,
-     semicolons: true },
-  module: undefined,
-  sourceMap: null,
-  toplevel: undefined,
-  nameCache: undefined,
-  ie8: undefined,
-  keep_classnames: undefined,
-  keep_fnames: undefined,
-  safari10: undefined }
 const file = fs.readFileSync('input.js', "utf8")
-const result = Terser.minify(file, options)
+const result = Terser.minify(file)
 console.log(result.code)

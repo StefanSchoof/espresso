84e9e0a47c73b001a18c7934babd371365558808
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-02-01 17:40:38 +0100

    Move to new application insight package (#55)

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,19 +1,6 @@
-import { AppInsights } from "applicationinsights-js";
-
 declare const instrumentationKey: string;
-
-AppInsights.downloadAndSetup!({
-    // Currently defaults to true, see https://github.com/Microsoft/ApplicationInsights-JS/issues/395
-    disableCorrelationHeaders: false,
-    enableCorsCorrelation: true,
-    instrumentationKey,
-});
-
-// add support for fetch, see https://github.com/Microsoft/ApplicationInsights-JS/issues/625
-import { initAppInsightsFetchMonitor } from "application-insights-fetch-monitor";
-initAppInsightsFetchMonitor();
-
-AppInsights.trackPageView();
+declare const functionsCode: string;
+declare const functionsHostname: string;
 
 import { init } from "./main";
-init();
+init(functionsHostname, functionsCode, instrumentationKey);
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,60 +1,70 @@
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+
 declare var process: {
     env: {
         NODE_ENV: string;
     },
 };
 
-declare const functionsCode: string;
-declare const functionsHostname: string;
-
-const serviceUrl = process.env.NODE_ENV === "development" ? "/" : `https://${functionsHostname}/`;
-
-function createButton(arg: "on" | "off", title: string): HTMLButtonElement {
-    const button = document.createElement("button");
-    button.style.height = "100px";
-    button.style.width = "200px";
-    button.style.fontSize = "60px";
-    button.textContent = title;
-    button.addEventListener("click", async () => {
-        const status = document.getElementById("status") as HTMLSpanElement;
-        status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`;
-        try {
-            const res = await fetch(`${serviceUrl}api/switch?${arg}=1&code=${functionsCode}`, {method: "POST"});
-            status.textContent = res.ok ?
-                `Maschine ${title.toLocaleLowerCase()}` :
-                `Fehler vom Service: ${await res.text()}`;
-        } catch (err) {
-            status.textContent = `Netzwerkfehler: ${err.message}`;
-        }
+export function init(functionsHostname: string, functionsCode: string, instrumentationKey: string) {
+    const appInsights = new ApplicationInsights({
+        config: {
+           disableFetchTracking: false,
+           enableCorsCorrelation: true,
+           instrumentationKey,
+        },
     });
-    return button;
-}
+    appInsights.loadAppInsights();
 
-async function warmUp(): Promise<void> {
-    const status = document.getElementById("status")!;
-    status.textContent = "Aufwärmen...";
-    try {
-        await fetch(`${serviceUrl}api/switch`);
-        status.textContent = "Aufgewärmt";
-    } catch {
-        status.textContent = "Fehler beim Aufwärmen";
+    const serviceUrl = process.env.NODE_ENV === "development" ? "/" : `https://${functionsHostname}/`;
+
+    function createButton(arg: "on" | "off", title: string): HTMLButtonElement {
+        const button = document.createElement("button");
+        button.style.height = "100px";
+        button.style.width = "200px";
+        button.style.fontSize = "60px";
+        button.textContent = title;
+        button.addEventListener("click", async () => {
+            const status = document.getElementById("status") as HTMLSpanElement;
+            status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`;
+            try {
+                const res = await fetch(`${serviceUrl}api/switch?${arg}=1&code=${functionsCode}`, {method: "POST"});
+                status.textContent = res.ok ?
+                    `Maschine ${title.toLocaleLowerCase()}` :
+                    `Fehler vom Service: ${await res.text()}`;
+            } catch (err) {
+                status.textContent = `Netzwerkfehler: ${err.message}`;
+            }
+        });
+        return button;
     }
-}
 
-function component() {
-    const div = document.createElement("div");
-    div.appendChild(createButton("on", "An"));
-    div.appendChild(document.createElement("br"));
-    div.appendChild(createButton("off", "Aus"));
-    div.appendChild(document.createElement("br"));
-    const span = document.createElement("span");
-    span.id = "status";
-    div.appendChild(span);
+    async function warmUp(): Promise<void> {
+        const status = document.getElementById("status")!;
+        status.textContent = "Aufwärmen...";
+        try {
+            await fetch(`${serviceUrl}api/switch`);
+            status.textContent = "Aufgewärmt";
+        } catch {
+            status.textContent = "Fehler beim Aufwärmen";
+        }
+    }
 
-    return div;
-}
+    function component() {
+        const div = document.createElement("div");
+        div.appendChild(createButton("on", "An"));
+        div.appendChild(document.createElement("br"));
+        div.appendChild(createButton("off", "Aus"));
+        div.appendChild(document.createElement("br"));
+        const span = document.createElement("span");
+        span.id = "status";
+        div.appendChild(span);
 
-export function init() {
+        return div;
+    }
+
+    appInsights.trackPageView({name: "index"});
+
     document.body.appendChild(component());
     warmUp();
 }

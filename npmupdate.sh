11505b37141ffffee402aaebba930ab30d48270d
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-03-17 11:10:33 +0000

    update npm packages

--- npmupdate.sh
+++ npmupdate.sh
@@ -5,6 +5,11 @@ then
   exit 1
 fi
 
+if [ "$1" = "-deleteBranch" ]
+then
+  git branch -d npmupdate
+fi
+
 function execForAll
 {
   find . -maxdepth 2 -name package.json -execdir "$@" \;
@@ -14,5 +19,6 @@ execForAll npm install
 execForAll npm update
 execForAll rm -rf package-lock.json node_modules
 execForAll npm install-test
+execForAll npm run build
 git checkout -b npmupdate
 git add .

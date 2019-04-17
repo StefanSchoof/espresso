8b5c45ac590425a4fa8b27937059b4c705c089db
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-04-17 15:31:27 +0000

    use platform argument

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 set -e
-function pullarmimage {
-  # since the platform arg needs experimental daemon and there is no way do activate experimental daemon on azure pipeline => a hack...
-  echo "pull armv6 image for $1"
-  imagename=${1%:*}
-  digest=$(DOCKER_CLI_EXPERIMENTAL="enabled" docker manifest inspect $1 | jq --raw-output '.manifests[] | select(.platform.architecture == "arm" and .platform.variant == "v6").digest')
-  [[ -z "$digest" ]] && >&2 echo "found no arm image" && exit 1
-  docker pull $imagename@$digest
-  docker tag $imagename@$digest $1
-}
 
 function buildtarget {
   tag=$image:build-cache-$1
   if [[ ! -z "$USECACHEFROM" ]]
   then
     cachefrom+="--cache-from $tag "
-    docker pull $tag || true
+    docker pull --platform linux/arm/v6 $tag || true
   fi
   docker build --target $1 \
+    --platform linux/arm/v6 \
     $cachefrom \
     $dockerfilearg \
     -t $tag .
@@ -29,15 +21,10 @@ function buildtarget {
 
 # in azure pipeline git is in detached head so git does not know it brach and we take the env var. But these are in the format /ref/head/master, so we take the basename
 branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})
-echo "##vso[task.setvariable variable=branchname]$branch"
 image="stefanschoof/espresso"
 # enable cross compile with on not arm devices
 if [[ ! $(uname -m) == arm* ]]
 then
-  for baseimage in $(sed -n 's/^FROM \([^ ]*\) .*/\1/p' Dockerfile | sort --uniq)
-  do
-    pullarmimage $baseimage
-  done
   dockerfilearg="-f Dockerfile_x86"
   sed 's/#x86only //' Dockerfile > Dockerfile_x86
   buildtarget "qemu"
@@ -65,6 +52,7 @@ then
 fi
 
 docker build \
+  --platform linux/arm/v6 \
   $cachefrom \
   $dockerfilearg \
   $buildtag \

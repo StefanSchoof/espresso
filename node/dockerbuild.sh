ee2c7c0431515970d3fc9788ceab712f3d349078
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-04-26 20:42:03 +0000

    try buildkit

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -1,58 +1,35 @@
 #!/bin/bash
 set -e
 
-function buildtarget {
-  tag=$image:build-cache-$1
-  if [[ ! -z "$USECACHEFROM" ]]
-  then
-    cachefrom+="--cache-from $tag "
-    docker pull $tag || true
-  fi
-  docker build --target $1 \
-    $cachefrom \
-    -t $tag .
-  if [[ ! -z "$PUSH" ]]
-  then
-    docker push $tag
-  fi
-}
-
 # in azure pipeline git is in detached head so git does not know it brach and we take the env var. But these are in the format /ref/head/master, so we take the basename
 branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})
 image="stefanschoof/espresso"
-# enable cross compile with on not arm devices
-if [[ ! $(uname -m) == arm* ]]
-then
-  docker run --rm --privileged linuxkit/binfmt:v0.7
-fi
 
-# cache-from does not work with multistage, see https://github.com/moby/moby/issues/34715
-buildtarget "cppbuilder"
-buildtarget "builder"
-
-if [[ ! -z "$USECACHEFROM" ]]
-then
-  cachefrom+="--cache-from $image:$branch"
-  docker pull $image:$branch || true
-fi
-
-container=$(docker create --name builder $image:build-cache-builder)
+buildctl build \
+  --frontend dockerfile.v0 \
+  --local dockerfile=. \
+  --local context=. \
+  --import-cache type=registry,ref=docker.io/$image:cache \
+  --opt target=builder \
+  --output type=docker,name=builder | docker load
+container=$(docker create --name builder builder)
 docker cp $container:/usr/src/app/junit.xml .
 docker cp $container:/usr/src/app/coverage .
 docker rm $container
+buildctl build \
+  --frontend dockerfile.v0 \
+  --local dockerfile=. \
+  --local context=. \
+  --import-cache type=registry,ref=docker.io/$image:cache \
+  --export-cache type=registry,ref=docker.io/$image:cache \
+  --output type=image,name=docker.io/$image:$branch,push=true
 
 if [[ ! -z "$BUILD_BUILDID" ]]
 then
-  buildtag="-t $image:build$BUILD_BUILDID "
-fi
-
-docker build \
-  $cachefrom \
-  $buildtag \
-  -t $image:$branch .
-
-if [[ ! -z "$PUSH" ]]
-then
-  docker push $image:${branch}
-  docker push $image:build${BUILD_BUILDID}
+  buildctl build \
+  --frontend dockerfile.v0 \
+  --local dockerfile=. \
+  --local context=. \
+  --import-cache type=registry,ref=docker.io/$image:cache \
+  --output type=image,name=docker.io/$image:$BUILD_BUILDID,push=true
 fi
---
+++ node/startbuildkitd.sh
@@ -0,0 +1,4 @@
+docker run --privileged linuxkit/binfmt:v0.7
+docker run -d --privileged -p 1234:1234 --name buildkit moby/buildkit:latest  --addr tcp://0.0.0.0:1234  --oci-worker-platform linux/arm/v6
+sudo docker cp buildkit:/usr/bin/buildctl /usr/bin/
+export BUILDKIT_HOST=tcp://0.0.0.0:1234

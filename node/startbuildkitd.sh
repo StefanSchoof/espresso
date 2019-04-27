6d28e9ac6ab9141456a45958ee73427f793d3c71
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-04-27 15:50:24 +0000

    remove debug output

--- node/startbuildkitd.sh
+++ node/startbuildkitd.sh
@@ -1,9 +1,5 @@
-docker version
-echo "activate binfmt"
 docker run --privileged linuxkit/binfmt:v0.7
-echo "start buildkitd"
 docker rm --force buildkit || true
 docker run -d --privileged -p 1234:1234 --name buildkit moby/buildkit:latest  --addr tcp://0.0.0.0:1234  --oci-worker-platform linux/arm/v6
-echo "copy buildctl"
 sudo docker cp buildkit:/usr/bin/buildctl /usr/bin/
 export BUILDKIT_HOST=tcp://0.0.0.0:1234

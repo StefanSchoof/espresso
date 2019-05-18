49fa6df6b77204b25181c96727aafede8f11a869
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-05-18 18:57:00 +0000

    show docker log

--- node/pideploy.sh
+++ node/pideploy.sh
@@ -7,3 +7,5 @@ APPINSIGHTS_INSTRUMENTATIONKEY=$2
 EOF
 # docker compose use stderr for normal messages and devops shows them as errors
 TAG=build$3 TestingCommand=$4 docker-compose --no-ansi up -d 2>&1
+sleep 10
+docker-compose logs

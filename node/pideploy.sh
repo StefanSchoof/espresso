a759882d502671bf87dad24c85cc386f5cd37fbb
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-05-18 19:09:22 +0000

    add testing cmd

--- node/pideploy.sh
+++ node/pideploy.sh
@@ -8,4 +8,4 @@ EOF
 # docker compose use stderr for normal messages and devops shows them as errors
 TAG=build$3 TestingCommand=$4 docker-compose --no-ansi up -d 2>&1
 sleep 10
-docker-compose logs
+TestingCommand=$4 docker-compose logs

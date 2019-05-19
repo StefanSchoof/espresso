8ab1de4e08443642f69eec9be619b4e391a6feff
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2019-05-19 08:40:02 +0000

    use buildkit version

--- node/startbuildkitd.sh
+++ node/startbuildkitd.sh
@@ -2,6 +2,6 @@
 set -e
 
 docker rm --force buildkit || true
-docker run -d --privileged -p 1234:1234 --name buildkit moby/buildkit:latest  --addr tcp://0.0.0.0:1234  --oci-worker-platform linux/arm/v6
+docker run -d --privileged -p 1234:1234 --name buildkit moby/buildkit:v0.5.0  --addr tcp://0.0.0.0:1234  --oci-worker-platform linux/arm/v6
 sudo docker cp buildkit:/usr/bin/buildctl /usr/bin/
 export BUILDKIT_HOST=tcp://0.0.0.0:1234

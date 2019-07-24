736bc79e0a4688a6a6baf5cf6cab21ef97947fb9
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-07-24 21:36:47 +0000

    try az cli again

--- deploy/backend.tf
+++ deploy/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "espresso-tfstate"
     storage_account_name = "espressotfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"

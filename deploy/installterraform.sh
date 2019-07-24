3d32e0c4cf5bd03c634f50da7ab04fbf8f7e0435
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-07-24 18:52:45 +0200

    Use keyserver pool

--- deploy/installterraform.sh
+++ deploy/installterraform.sh
@@ -11,7 +11,7 @@ cd $TMPDIR
 version=${1:-$(curl https://checkpoint-api.hashicorp.com/v1/check/terraform | jq '.current_version' -r)}
 path="https://releases.hashicorp.com/terraform/$version"
 # fingerprint taken from https://www.hashicorp.com/security.html
-gpg --keyserver pgp.mit.edu --recv-keys "91A6 E7F8 5D05 C656 30BE F189 5185 2D87 348F FC4C"
+gpg --keyserver pool.sks-keyservers.net --recv-keys "91A6 E7F8 5D05 C656 30BE F189 5185 2D87 348F FC4C"
 curl --fail -Os "$path/terraform_${version}_linux_amd64.zip"
 curl --fail -Os "$path/terraform_${version}_SHA256SUMS.sig"
 curl --fail -Os "$path/terraform_${version}_SHA256SUMS"

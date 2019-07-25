cee5e8e899655a7d9e279f5f0c41555a6bfe32e5
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-07-25 03:37:51 +0000

    no use of keysever

--- deploy/installterraform.sh
+++ deploy/installterraform.sh
@@ -7,11 +7,10 @@ function cleanup {
 }
 
 trap cleanup EXIT
+gpg --import hashicorp.asc
 cd $TMPDIR
 version=${1:-$(curl https://checkpoint-api.hashicorp.com/v1/check/terraform | jq '.current_version' -r)}
 path="https://releases.hashicorp.com/terraform/$version"
-# fingerprint taken from https://www.hashicorp.com/security.html
-gpg --keyserver pool.sks-keyservers.net --recv-keys "91A6 E7F8 5D05 C656 30BE F189 5185 2D87 348F FC4C"
 curl --fail -Os "$path/terraform_${version}_linux_amd64.zip"
 curl --fail -Os "$path/terraform_${version}_SHA256SUMS.sig"
 curl --fail -Os "$path/terraform_${version}_SHA256SUMS"

d35190e1fb6d412e327da6dfb241198e254f5afd
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-08-02 06:29:08 +0000

    remove debug output

--- deploy/writeoutputtodevops.sh
+++ deploy/writeoutputtodevops.sh
@@ -1,12 +1,4 @@
 . bashfunctions.sh
 
 export TF_WORKSPACE=${RELEASE_ENVIRONMENTNAME:-test}
-echo "one is $1"
-echo "two.is $2"
-echo "pwd is $PWD"
-setTerraformVars
-printenv | sort
-#ls -laR
-#terraform refresh
-terraform output
 writeDevopsVar "$1" "$(terraform output $1)" "" true

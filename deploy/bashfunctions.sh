fbdc3b88878035eacac776066f1589b527f66332
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-08-03 19:25:57 +0000

    split deployment into own task

--- deploy/bashfunctions.sh
+++ deploy/bashfunctions.sh
@@ -9,16 +9,20 @@ function writeDevopsVar {
   echo "##vso[task.setvariable variable=$1$secret]$2"
 }
 
-function initTerraform {
+function setTerraformVars {
   # taking the vars from the azure Devops task
   export ARM_CLIENT_ID="$servicePrincipalId"
   export ARM_CLIENT_SECRET="$servicePrincipalKey"
   export ARM_SUBSCRIPTION_ID="$(az account show --query id --output tsv)"
-  export ARM_TENANT_ID="$(az account show --query tenantId --output tsv)" # after task is updated to 1.152.3 use "$tenantId"
+  export ARM_TENANT_ID="$tenantId"
   export TF_IN_AUTOMATION=true
   if [[ -z $servicePrincipalId ]]
   then
     export TF_VAR_object_id=$(az ad signed-in-user show --query objectId --output tsv)
   fi
+}
+
+function initTerraform {
+  setTerraformVars
   terraform init -lock-timeout=50m -input=false
 }

3315ef72e83faca4fe48c0f45740c281ba7267f3
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-08-05 03:50:49 +0000

    show docker infos

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -3,7 +3,9 @@ set -e
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker -v
-docker buildx ls
+#DOCKER_CLI_EXPERIMENTAL=enabled docker buildx ls
+docker version
+docker info
 
 # in azure pipeline git is in detached head so git does not know it brach and we take the env var. But these are in the format /ref/head/master, so we take the basename
 branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})

2b186db348895fa1fa3ea48ffa2c9db7a535223b
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-08-06 06:03:49 +0000

    use bake

--- node/dockerbuild.sh
+++ node/dockerbuild.sh
@@ -3,7 +3,7 @@ set -e
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 docker -v
-#DOCKER_CLI_EXPERIMENTAL=enabled docker buildx ls
+DOCKER_CLI_EXPERIMENTAL=enabled docker buildx ls
 docker version
 docker info
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -15,28 +15,28 @@ docker info
 # in azure pipeline git is in detached head so git does not know it brach and we take the env var. But these are in the format /ref/head/master, so we take the basename
 branch=$(basename ${BUILD_SOURCEBRANCH:-$(git rev-parse --abbrev-ref HEAD)})
 image="stefanschoof/espresso"
-
-buildctl build \
-  --frontend dockerfile.v0 \
-  --local dockerfile=. \
-  --local context=. \
-  --import-cache type=registry,ref=docker.io/$image:cache \
-  --export-cache type=registry,ref=docker.io/$image:cache,mode=max \
-  --output type=image,name=docker.io/$image:$branch,push=true
-buildctl build \
-  --frontend dockerfile.v0 \
-  --local dockerfile=. \
-  --local context=. \
-  --import-cache type=registry,ref=docker.io/$image:cache \
-  --opt target=testresult \
-  --output type=local,dest=.
-
-if [[ ! -z "$BUILD_BUILDID" ]]
-then
-  buildctl build \
-    --frontend dockerfile.v0 \
-    --local dockerfile=. \
-    --local context=. \
-    --import-cache type=registry,ref=docker.io/$image:cache \
-    --output type=image,name=docker.io/$image:build$BUILD_BUILDID,push=true
-fi
+docker buildx bake
+#buildctl build \
+#  --frontend dockerfile.v0 \
+#  --local dockerfile=. \
+#  --local context=. \
+#  --import-cache type=registry,ref=docker.io/$image:cache \
+#  --export-cache type=registry,ref=docker.io/$image:cache,mode=max \
+#  --output type=image,name=docker.io/$image:$branch,push=true
+#buildctl build \
+#  --frontend dockerfile.v0 \
+#  --local dockerfile=. \
+#  --local context=. \
+#  --import-cache type=registry,ref=docker.io/$image:cache \
+#  --opt target=testresult \
+#  --output type=local,dest=.
+#
+#if [[ ! -z "$BUILD_BUILDID" ]]
+#then
+#  buildctl build \
+#    --frontend dockerfile.v0 \
+#    --local dockerfile=. \
+#    --local context=. \
+#    --import-cache type=registry,ref=docker.io/$image:cache \
+#    --output type=image,name=docker.io/$image:build$BUILD_BUILDID,push=true
+#fi

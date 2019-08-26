ce852569cec38df8df8272d088458ad1cf87cf82
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-08-26 19:04:47 +0000

    get iothub nane

--- deploy/iac.sh
+++ deploy/iac.sh
@@ -8,6 +8,7 @@ function writeKeyVault
 {
   echo "write to keyvault"
   az extension add --name azure-cli-iot-ext
+  iothub=$(terraform output iothub)
   deviceConnectionString="$(az iot hub device-identity show-connection-string --hub-name $iothub --device-id espressoPi --output tsv)"
   writeDevopsVar DeviceConnectionString "$deviceConnectionString" true
   az keyvault secret set --vault-name "$KEYVAULTNAME" --name 'DeviceConnectionString' --value "$deviceConnectionString"

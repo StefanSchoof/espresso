06cff8c0eafa66688b2ea415c9c5c4d75fe289a3
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-15 20:46:25 +0000

    silence login

--- deploy/iotdeviceconnection.sh
+++ deploy/iotdeviceconnection.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if ! az account show > /dev/null
 then
-  az login --service-principal -u $ARM_CLIENT_ID -p  $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+  az login --service-principal -u $ARM_CLIENT_ID -p  $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
 fi
 if ! az iot hub device-identity show-connection-string --hub-name $1 --device-id espressoPi 2>/dev/null
 then

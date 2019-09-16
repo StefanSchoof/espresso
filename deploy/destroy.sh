3641b0c359385171b43851777b45cc3e89d1b7e7
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-16 03:21:34 +0000

    destroy with vars

--- deploy/destroy.sh
+++ deploy/destroy.sh
@@ -5,5 +5,5 @@ set -e
 # destroy costly resouces in test
 export TF_WORKSPACE="test"
 initTerraform
-terraform destroy -target azurerm_virtual_machine.dockerhost -target azurerm_iothub.iothub -auto-approve
+terraform destroy -target azurerm_virtual_machine.dockerhost -target azurerm_iothub.iothub -auto-approve -var-file=test.tfvars
 

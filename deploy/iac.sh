0dcb96727b8e3f00382a83afab01809564d0ab89
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-16 19:04:57 +0000

    remove keyvault

--- deploy/iac.sh
+++ deploy/iac.sh
@@ -7,9 +7,6 @@ export TF_WORKSPACE=${RELEASE_ENVIRONMENTNAME:-test}
 echo "write to keyvault"
 deviceConnectionString="$(terraform output iot_device_connection_string)"
 writeDevopsVar DeviceConnectionString "$deviceConnectionString" true true
-az keyvault secret set --vault-name "$KEYVAULTNAME" --name 'DeviceConnectionString' --value "$deviceConnectionString"
 nodeInstrumentationKey="$(terraform output azurerm_application_insights_node)"
 writeDevopsVar NodeInstrumentationKey "$nodeInstrumentationKey" true true
-az keyvault secret set --vault-name "$KEYVAULTNAME" --name NodeInstrumentationKey --value "$nodeInstrumentationKey"
 websiteUrl=$(terraform output static-web-url)
-az keyvault secret set --vault-name "$KEYVAULTNAME" --name WebsiteUrl --value "$websiteUrl"

7294b820807623d4ba79445091624ef8c1f98590
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-16 13:35:28 +0000

    load extension

--- deploy/iotdeviceconnection.sh
+++ deploy/iotdeviceconnection.sh
@@ -4,4 +4,5 @@ if ! az account show > /dev/null 2>&1
 then
   az login --service-principal -u $ARM_CLIENT_ID -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID > /dev/null
 fi
+az extension add --name azure-cli-iot-ext
 az iot hub device-identity show-connection-string --hub-name $1 --device-id espressoPi

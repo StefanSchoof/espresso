5292960c06f0792c5b60ff3c29c8c5026bb85b7d
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-21 15:54:18 +0000

    use terraform output json

---
+++ deploy/output.ps1
@@ -0,0 +1,8 @@
+Write-Host $env:jsonOutputVariablesPath
+$json=Get-Content $env:jsonOutputVariablesPath | ConvertFrom-Json
+$json `
+    | Get-Member -type NoteProperty `
+    | ForEach-Object {
+        $o=$json."$($_.Name)"
+        "##vso[task.setvariable variable=$($_.Name);isOutput=true$(if ($o.sensitive -eq "True") {";issecret=true"})]$($o.value)"
+    }

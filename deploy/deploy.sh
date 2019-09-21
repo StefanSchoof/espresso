48849c6d57e0f5579b10b35acb33d7e550f52178
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-21 21:18:03 +0200

    use uppercase

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,10 +2,12 @@
 set -e
 . ./bashfunctions.sh
 
+printenv | sort
+
 function deployFunc {
   pushd ../func
   res=$(npx -p azure-functions-core-tools \
-    func azure functionapp publish $function_app)
+    func azure functionapp publish $FUNCTION_APP)
   popd
   FUNCTIONS_CODE=$(echo "$res" | sed 's/^.*code=//;t;d')
   [[ -z "$FUNCTIONS_CODE" ]] && echo "##vso[task.logissue type=error]found no functionscode" && exit 1
@@ -16,9 +18,9 @@ function deployFunc {
 
 function deployWeb {
   sed -i \
-      -e "s/<%INSTRUMENTATION_KEY%>/${azurerm_application_insights_web}/" \
+      -e "s/<%INSTRUMENTATION_KEY%>/${AZURERM_APPLICATION_INSIGHTS_WEB}/" \
       -e "s/<%FUNCTIONS_CODE%>/${FUNCTIONS_CODE//\//\\/}/" `#the many / and \ escape possible /` \
-      -e "s/<%FUNCTIONS_HOSTNAME%>/${function_app_hostname}/" \
+      -e "s/<%FUNCTIONS_HOSTNAME%>/${FUNCTION_APP_HOSTNAME}/" \
       ../web/index.html
   az storage blob upload-batch -s ../web -d '$web' --account-name $storage_account
 }

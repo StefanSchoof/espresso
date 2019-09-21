850233b0e16f5f2d44259f3003058a44a567647b
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-21 21:03:00 +0200

    use terrafrom vars from json

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,13 +2,10 @@
 set -e
 . ./bashfunctions.sh
 
-export TF_WORKSPACE=${RELEASE_ENVIRONMENTNAME:-test}
-
 function deployFunc {
-  functionAppName=$(terraform output function_app)
   pushd ../func
   res=$(npx -p azure-functions-core-tools \
-    func azure functionapp publish $functionAppName)
+    func azure functionapp publish $function_app)
   popd
   FUNCTIONS_CODE=$(echo "$res" | sed 's/^.*code=//;t;d')
   [[ -z "$FUNCTIONS_CODE" ]] && echo "##vso[task.logissue type=error]found no functionscode" && exit 1
@@ -18,17 +15,13 @@ function deployFunc {
 }
 
 function deployWeb {
-  INSTRUMENTATION_KEY=$(terraform output azurerm_application_insights_web)
-  FUNCTIONS_HOSTNAME=$(terraform output function_app_hostname)
-  STORAGE_ACCOUNT_NAME=$(terraform output storage_account)
   sed -i \
-      -e "s/<%INSTRUMENTATION_KEY%>/${INSTRUMENTATION_KEY}/" \
+      -e "s/<%INSTRUMENTATION_KEY%>/${azurerm_application_insights_web}/" \
       -e "s/<%FUNCTIONS_CODE%>/${FUNCTIONS_CODE//\//\\/}/" `#the many / and \ escape possible /` \
-      -e "s/<%FUNCTIONS_HOSTNAME%>/${FUNCTIONS_HOSTNAME}/" \
+      -e "s/<%FUNCTIONS_HOSTNAME%>/${function_app_hostname}/" \
       ../web/index.html
-  az storage blob upload-batch -s ../web -d '$web' --account-name $STORAGE_ACCOUNT_NAME
+  az storage blob upload-batch -s ../web -d '$web' --account-name $storage_account
 }
 
-setTerraformVars
 deployFunc
 deployWeb
--- deploy/writeoutputtodevops.sh
+++
@@ -1,5 +0,0 @@
-. bashfunctions.sh
-
-export TF_WORKSPACE=${RELEASE_ENVIRONMENTNAME:-test}
-setTerraformVars
-writeDevopsVar "$1" "$(terraform output $1)" "" true

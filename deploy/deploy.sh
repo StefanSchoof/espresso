5687a924e13d9d23c2cec6cea8be54dc07e6bc46
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-09-22 17:52:39 +0200

    destroy with terraform task (#163)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
-. ./bashfunctions.sh
+
+function writeDevopsVar {
+  options=""
+  if [ $3 ]
+  then
+    options=";issecret=true"
+  fi
+  if [ $4 ]
+  then
+    options="$options;isOutput=true"
+  fi
+  echo "##vso[task.setvariable variable=$1$options]$2"
+}
 
 function deployFunc {
   pushd ../func
--- deploy/destroy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-. bashfunctions.sh
-
-# destroy costly resouces in test
-export TF_WORKSPACE="test"
-initTerraform
-terraform destroy -target azurerm_virtual_machine.dockerhost -target azurerm_iothub.iothub -auto-approve -var-file=test.tfvars
-

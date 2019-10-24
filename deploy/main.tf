c3159b35e8943437fe31606f2264ab741db16a01
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-10-24 03:25:32 +0000

    set node version to latest of 10

--- deploy/main.tf
+++ deploy/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_function_app" "function" {
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "node"
     WEBSITE_RUN_FROM_PACKAGE       = "1"
-    WEBSITE_NODE_DEFAULT_VERSION   = "10.14.1"
+    WEBSITE_NODE_DEFAULT_VERSION   = "~10"
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.function.instrumentation_key
     IOTHUB_CONNECTION_STRING       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.iotHubConnectionString.id})"
   }

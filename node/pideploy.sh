d0dc44cb4561ab36da91e41a467643c7f3f99727
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-11-02 21:47:04 +0100

    pull image by generic tag name (#175)

--- node/pideploy.sh
+++ node/pideploy.sh
@@ -6,4 +6,5 @@ CONNECTION_STRING=$1
 APPINSIGHTS_INSTRUMENTATIONKEY=$2
 EOF
 # docker compose use stderr for normal messages and devops shows them as errors
-TAG=build$3 TestingCommand=$4 docker-compose --no-ansi up -d 2>&1
+TAG=$3 TestingCommand=$4 docker-compose --no-ansi pull 2>&1
+TAG=$3 TestingCommand=$4 docker-compose --no-ansi up -d 2>&1

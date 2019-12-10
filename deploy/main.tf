f3869f095d6640ad4d2e0c44c444137bb60bd01c
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2019-12-10 06:37:35 +0100

    Upgrade to azure function 3 (#180)

--- deploy/main.tf
+++ deploy/main.tf
@@ -96,11 +96,11 @@ resource "azurerm_function_app" "function" {
   app_settings = {
     FUNCTIONS_WORKER_RUNTIME       = "node"
     WEBSITE_RUN_FROM_PACKAGE       = "1"
-    WEBSITE_NODE_DEFAULT_VERSION   = "~10"
+    WEBSITE_NODE_DEFAULT_VERSION   = "~12"
     APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.function.instrumentation_key
     IOTHUB_CONNECTION_STRING       = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.iotHubConnectionString.id})"
   }
-  version = "~2"
+  version = "~3"
 }
 
 resource "azurerm_key_vault" "keyvault" {

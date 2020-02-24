bc36288d10a20fbaf767a4dcafb42191d21d3d0f
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2020-02-24 20:47:02 +0000

    move public key into var

--- deploy/testvm.tf
+++ deploy/testvm.tf
@@ -76,7 +76,7 @@ resource "azurerm_linux_virtual_machine" "dockerhost" {
   custom_data    = data.template_cloudinit_config.config.rendered
   admin_ssh_key {
     username   = "dockeradmin"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9QKIJL4OygWMvTzvsi9zC03R/t5riw4SfLg3+EqfM79Bex0ChBdqx6i2ddhkmkfGwFXm/Si2cEM2WVcjNWCgbTgKaJDGVANnCz4zlxqsUAEE2izzMLD+vLqSz7OAn/xAiMv0w0mNevmleFLPwgRyXCq7TRzl+b83/DJD6R4YIHeqsnCRkqCmh+FGJL9SF0u+gIdl8/a4L0XLTz2nvWVrFWPfP4bn5f6GCKpKuaHwa9dxyGlQRo1xYviE5nRZjxVfY4cBvahkBJFxc26iRVDgdyqk/iTPVosN2qNDQ3/Yt2106UqRmLi9ssW6hiFr2Ejoq3JJd6Tq8V2QRVU45OQEV sschoof@sdhamw057"
+    public_key = var.public_key
   }
 }
 

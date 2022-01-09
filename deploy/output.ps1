df2b05ce42d290bea68d9fa6ded455979a4a7716
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2022-01-09 12:18:10 +0100

    remove no longer needed files

--- deploy/output.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Convert terrafrom output from $env:jsonPath to devops vars."
-$json=Get-Content $env:jsonPath | ConvertFrom-Json
-$json `
-    | Get-Member -type NoteProperty `
-    | ForEach-Object {
-        $o=$json."$($_.Name)"
-        "##vso[task.setvariable variable=$($_.Name);isOutput=true$(if ($o.sensitive -eq "True") {";issecret=true"})]$($o.value)"
-    }

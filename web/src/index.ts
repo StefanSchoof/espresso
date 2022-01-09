33d0d5e6b0a59f71efc22df9e411789c30704726
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2022-01-09 12:10:36 +0100

    use docker to serve static files on rpi

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,6 +1,2 @@
-declare const instrumentationKey: string;
-declare const functionsCode: string;
-declare const functionsHostname: string;
-
 import { init } from "./main";
-init(functionsHostname, functionsCode, instrumentationKey);
+init();
--- web/src/main.test.ts
+++ web/src/main.test.ts
@@ -3,8 +3,7 @@ import { init } from "./main";
 declare function setImmediate(cb: () => void): void;
 const immediate = (): Promise<void> => new Promise((resolve) => setImmediate(resolve));
 
-function getButton(name: string, buttons: HTMLButtonElement[]): HTMLButtonElement
-{
+function getButton(name: string, buttons: HTMLButtonElement[]): HTMLButtonElement {
     const button = buttons.find((e) => e.textContent === name);
     if (!button) {
         throw `Found no button '${name}'`;
@@ -14,7 +13,7 @@ function getButton(name: string, buttons: HTMLButtonElement[]): HTMLButtonElemen
 
 describe("main", () => {
     beforeAll(() => {
-        window.fetch = jest.fn(() => Promise.resolve({ok: true})) as any;
+        window.fetch = jest.fn(() => Promise.resolve({ ok: true })) as any;
     });
 
     describe("click", () => {
@@ -26,7 +25,7 @@ describe("main", () => {
                 document.body.removeChild(document.body.firstChild);
             }
             jest.clearAllMocks();
-            init("func.azurewebsites.net", "abc", "key");
+            init();
             const buttons = Array.from(document.body.querySelectorAll("button"));
             on = getButton("An", buttons);
             off = getButton("Aus", buttons);
@@ -42,7 +41,7 @@ describe("main", () => {
             await immediate();
 
             expect(window.fetch)
-                .toHaveBeenCalledWith(expect.stringContaining("azurewebsites.net/api/switch?on"), {method: "POST"});
+                .toHaveBeenCalledWith(expect.stringContaining("api/on"), { method: "POST" });
             expect((document.getElementById("status") as HTMLSpanElement).textContent).toBe("Maschine an");
         });
 
@@ -52,7 +51,7 @@ describe("main", () => {
             await immediate();
 
             expect(window.fetch)
-                .toHaveBeenCalledWith(expect.stringContaining("azurewebsites.net/api/switch?off"), {method: "POST"});
+                .toHaveBeenCalledWith(expect.stringContaining("api/off"), { method: "POST" });
             expect((document.getElementById("status") as HTMLSpanElement).textContent).toBe("Maschine aus");
         });
 
@@ -67,35 +66,13 @@ describe("main", () => {
         });
 
         test("show error if request after on click is not ok", async () => {
-            window.fetch = jest.fn(() => Promise.resolve({ok: false, text: () => Promise.resolve("Not working")})) as any;
+            window.fetch = jest.fn(() => Promise.resolve({ ok: false, text: () => Promise.resolve("Not working") })) as any;
 
             on.click();
             await immediate();
 
             expect((document.getElementById("status") as HTMLSpanElement).textContent)
                 .toBe("Fehler vom Service: Not working");
-        });
-    });
-
-    describe("warmup", () => {
-        test("warms the function app on page load", async () => {
-            init("func.azurewebsites.net", "abc", "key");
-            expect(window.fetch).toHaveBeenCalledWith(expect.stringMatching(".azurewebsites.net/api/switch*$"));
-            expect((document.getElementById("status") as HTMLSpanElement).textContent).toBe("Aufwärmen...");
-
-            await immediate();
-
-            expect((document.getElementById("status") as HTMLSpanElement).textContent).toBe("Aufgewärmt");
-        });
-
-        test("shows an error if warm up fails", async () => {
-            window.fetch = jest.fn(() => { throw new Error("fetch faild"); });
-
-            init("func.azurewebsites.net", "abc", "key");
-
-            await immediate();
-
-            expect((document.getElementById("status") as HTMLSpanElement).textContent).toBe("Fehler beim Aufwärmen");
         });
     });
 });

f2fd655f11acfe62d632bb92f02930df848727f8
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2022-01-28 16:22:28 +0100

    Add e2e tests

---
+++ e2e/cypress/integration/main.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('esprsso', () => {
+  beforeEach(() => {
+    cy.visit('/')
+
+    cy.intercept('POST', '**/api/*', { statusCode: 200 }).as('api')
+  })
+
+  it('Send a on request on "An" click', () => {
+    cy.get('button:contains("An")').click()
+
+    cy.wait('@api').its('request.url')
+      .should('match', /.*\/api\/on/)
+  })
+
+  it('Send a off request on "Aus" click', () => {
+    cy.get('button:contains("Aus")').click()
+
+    cy.wait('@api').its('request.url')
+      .should('match', /.*\/api\/off/)
+  })
+})

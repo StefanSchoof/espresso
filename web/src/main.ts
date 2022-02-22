d9d10f2ac33eabb411a2c718d64efe13e580d260
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2022-02-22 08:15:49 +0100

    apply standardjs lint changes

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,43 +1,49 @@
-export function init(): void {
-    function createButton(arg: "on" | "off", title: string): HTMLButtonElement {
-        const button = document.createElement("button");
-        button.style.height = "100px";
-        button.style.width = "200px";
-        button.style.fontSize = "60px";
-        button.textContent = title;
-        button.addEventListener("click", async () => {
-            const status = document.getElementById("status");
-            if (!status) {
-                throw "Could not find status";
-            }
-            status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`;
-            try {
-                const res = await fetch(`api/${arg}`, { method: "POST" });
-                status.textContent = res.ok
-                    ? `Maschine ${title.toLocaleLowerCase()}`
-                    : `Fehler vom Service: ${await res.text()}`;
-            } catch (err) {
-                status.textContent =
-                    err instanceof Error
-                        ? `Netzwerkfehler: ${err.message}`
-                        : `Unbekannter Fehler: ${JSON.stringify(err)}`;
-            }
-        });
-        return button;
+export function init (): void {
+  async function send (arg: string, title: string): Promise<void> {
+    const status = document.getElementById('status')
+    if (status == null) {
+      throw new Error('Could not find status')
     }
-
-    function component(): HTMLDivElement {
-        const div = document.createElement("div");
-        div.appendChild(createButton("on", "An"));
-        div.appendChild(document.createElement("br"));
-        div.appendChild(createButton("off", "Aus"));
-        div.appendChild(document.createElement("br"));
-        const span = document.createElement("span");
-        span.id = "status";
-        div.appendChild(span);
-
-        return div;
+    status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`
+    try {
+      const res = await fetch(`api/${arg}`, { method: 'POST' })
+      status.textContent = res.ok
+        ? `Maschine ${title.toLocaleLowerCase()}`
+        : `Fehler vom Service: ${await res.text()}`
+    } catch (err) {
+      status.textContent =
+        err instanceof Error
+          ? `Netzwerkfehler: ${err.message}`
+          : `Unbekannter Fehler: ${JSON.stringify(err)}`
     }
+  }
 
-    document.body.appendChild(component());
+  function createButton (arg: 'on' | 'off', title: string): HTMLButtonElement {
+    const button = document.createElement('button')
+    button.style.height = '100px'
+    button.style.width = '200px'
+    button.style.fontSize = '60px'
+    button.textContent = title
+    button.addEventListener('click', (): void => {
+      send(arg, title).catch(
+        () => { }
+      )
+    })
+    return button
+  }
+
+  function component (): HTMLDivElement {
+    const div = document.createElement('div')
+    div.appendChild(createButton('on', 'An'))
+    div.appendChild(document.createElement('br'))
+    div.appendChild(createButton('off', 'Aus'))
+    div.appendChild(document.createElement('br'))
+    const span = document.createElement('span')
+    span.id = 'status'
+    div.appendChild(span)
+
+    return div
+  }
+
+  document.body.appendChild(component())
 }

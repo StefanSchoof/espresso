f40533d6adaeb5711ae9f43f514f0d16b36c2bc7
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2022-07-08 17:32:03 +0200

    standard lint

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents (on, config) {},
     baseUrl: 'http://host.docker.internal',
     supportFile: false
-  },
+  }
 })

6c9bb35d096fd9c4ef8e4a52ccf17569e204692b
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2023-10-30 17:52:14 +0100

    switch from ts standard to prettier (#729)
    * switch from ts standard to prettier
    
    * lint js also with prettier
    
    * disable ts for now
    
    * disable only ts standard

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,49 +1,47 @@
-export function init (): void {
-  async function send (arg: string, title: string): Promise<void> {
-    const status = document.getElementById('status')
+export function init(): void {
+  async function send(arg: string, title: string): Promise<void> {
+    const status = document.getElementById("status");
     if (status == null) {
-      throw new Error('Could not find status')
+      throw new Error("Could not find status");
     }
-    status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`
+    status.textContent = `Schalte Maschine ${title.toLocaleLowerCase()}`;
     try {
-      const res = await fetch(`api/${arg}`, { method: 'POST' })
+      const res = await fetch(`api/${arg}`, { method: "POST" });
       status.textContent = res.ok
         ? `Maschine ${title.toLocaleLowerCase()}`
-        : `Fehler vom Service: ${await res.text()}`
+        : `Fehler vom Service: ${await res.text()}`;
     } catch (err) {
       status.textContent =
         err instanceof Error
           ? `Netzwerkfehler: ${err.message}`
-          : `Unbekannter Fehler: ${JSON.stringify(err)}`
+          : `Unbekannter Fehler: ${JSON.stringify(err)}`;
     }
   }
 
-  function createButton (arg: 'on' | 'off', title: string): HTMLButtonElement {
-    const button = document.createElement('button')
-    button.style.height = '100px'
-    button.style.width = '200px'
-    button.style.fontSize = '60px'
-    button.textContent = title
-    button.addEventListener('click', (): void => {
-      send(arg, title).catch(
-        () => { }
-      )
-    })
-    return button
+  function createButton(arg: "on" | "off", title: string): HTMLButtonElement {
+    const button = document.createElement("button");
+    button.style.height = "100px";
+    button.style.width = "200px";
+    button.style.fontSize = "60px";
+    button.textContent = title;
+    button.addEventListener("click", (): void => {
+      send(arg, title).catch(() => {});
+    });
+    return button;
   }
 
-  function component (): HTMLDivElement {
-    const div = document.createElement('div')
-    div.appendChild(createButton('on', 'An'))
-    div.appendChild(document.createElement('br'))
-    div.appendChild(createButton('off', 'Aus'))
-    div.appendChild(document.createElement('br'))
-    const span = document.createElement('span')
-    span.id = 'status'
-    div.appendChild(span)
+  function component(): HTMLDivElement {
+    const div = document.createElement("div");
+    div.appendChild(createButton("on", "An"));
+    div.appendChild(document.createElement("br"));
+    div.appendChild(createButton("off", "Aus"));
+    div.appendChild(document.createElement("br"));
+    const span = document.createElement("span");
+    span.id = "status";
+    div.appendChild(span);
 
-    return div
+    return div;
   }
 
-  document.body.appendChild(component())
+  document.body.appendChild(component());
 }
